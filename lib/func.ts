42a0ae2181f847db0c7524586969830cb3a91d76
Yang <zhy0216@gmail.com>
2018-03-12 15:45:55 +0800

    add parse function parameters

--- lib/func.ts
+++ lib/func.ts
@@ -19,15 +19,29 @@ export class Func{
     closure: Dict<any>;
     lazyClosure: Dict<Func>;
     parameters: Array<Parameter>;
+    static STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
 
-    constructor(func, closure=null,){
+    constructor(func, closure=null, lazyClosure=null){
         this.func = func;
         this.closure = closure || {};
-        this.lazyClosure = {};
+        this.lazyClosure = lazyClosure || {};
         this.parameters = [];
+        this._parseFunc(func);
     }
 
-    _parseFunc(func){
+    _parseFunc(f){
+        const fnStr = f.toString().replace(Func.STRIP_COMMENTS, '');
+        const argsStr = fnStr.slice(fnStr.indexOf('(')+1, fnStr.indexOf(')'));
+        const argsList = argsStr.replace(/ /g, "").split(",");
+        for(let index in argsList){
+            let argName = argsList[index];
+            let equationIndex = argName.indexOf("=");
+            if(equationIndex === -1){
+                this.parameters.push(new Parameter(argName, true, index));
+            }else{
+                this.parameters.push(new Parameter(argName.slice(0, equationIndex), false, index));
+            }
+        }
 
     }
 
