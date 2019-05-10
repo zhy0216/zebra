8ad9f0f333982867fa46c641917658adf4a9a995
Yang <zhy0216@gmail.com>
2019-05-10 13:34:52 -0700

    remove js files

--- src/examples/hello.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("../lib/app");
-app_1.z.addGet("/hello/{name}", (name) => `hello, ${name}`);
-if (require.main === module) {
-    app_1.z.run();
-}
